562adb4f6d39c38f56aa75365d62c08efcdf89bd
leadpony <leadpony@openclosed.dev>
2026-01-11 17:16:58 +0900

    Upgrade gulp-autoprefixer to the latest

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 'use strict';
 
-var gulp = require('gulp');
-var gulpSass = require('gulp-sass');
-var gulpAutoPrefixer = require('gulp-autoprefixer');
-var browserSync = require('browser-sync').create();
+import gulp from 'gulp'
+import dartSass from 'sass'
+import gulpSass from 'gulp-sass'
+import gulpAutoPrefixer from 'gulp-autoprefixer'
+import browserSync from 'browser-sync'
 
-gulpSass = gulpSass(require('sass'));
+const bs = browserSync.create()
+const gs = gulpSass(dartSass)
 
 function sass(env) {
   var nodeSassOption = {
@@ -14,12 +16,12 @@ function sass(env) {
   };
   // sourceComments
   return gulp.src('./sass/*.scss')
-    .pipe(gulpSass(nodeSassOption).on('error', gulpSass.logError))
+    .pipe(gs(nodeSassOption).on('error', gs.logError))
     .pipe(gulpAutoPrefixer({
       cascade: true
     }))
     .pipe(gulp.dest('stylesheets'))
-    .pipe(browserSync.stream())
+    .pipe(bs.stream())
     ;
 }
 
@@ -32,7 +34,7 @@ gulp.task('sass:dev', function () {
 });
 
 gulp.task('browser-sync', function (done) {
-  browserSync.init({
+  bs.init({
     port: 3001,
     proxy: '127.0.0.1:3000'
   });
